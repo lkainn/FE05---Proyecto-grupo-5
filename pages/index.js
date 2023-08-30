744155c895efee1f2a6a66d8ab101dac53e091e6
lkainn <143043638+lkainn@users.noreply.github.com>
2023-08-30 19:33:18 -0300

    Agregado de sistema de login, registro y navegacion entre paginas

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Link from 'next/link';
 import LoginForm from '../components/LoginForm';
-import TasksPage from '../components/TasksPage';
 
 const Home = () => {
-  const [user, setUser] = useState(null);
-
-  const handleLogin = (loggedInUser) => {
-    setUser(loggedInUser);
-  };
-
-  const handleLogout = () => {
-    setUser(null);
-  };
-
   return (
     <div>
       <h1>Administrador de tareas</h1>
-      {user ? (
-        <TasksPage user={user} onLogout={handleLogout} />
-      ) : (
-        <LoginForm onLogin={handleLogin} />
-      )}
+      <LoginForm />
+      <p>No tenes cuenta? <Link href="/register">Registrate aca</Link></p>
     </div>
   );
 };
 
 export default Home;
+
